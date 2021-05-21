856a02eb5e30cb43104e48ebda9ad572bb09630a
2hard4me <usmanov.aa@phystech.edu>
2021-05-21 18:59:30 +0300

    Solved Generate_Parentheses problem

--- Top 100 Interview Questions/#22_Generate_Parentheses.cpp
+++ Top 100 Interview Questions/#22_Generate_Parentheses.cpp
@@ -1,1 +1,16 @@
-
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        if(n == 0)
+            return {""};
+        vector<string> ans;
+        for(int  i = 0; i < n ; ++i) {
+            for(auto& first : generateParenthesis(i)) {
+                for(auto& second : generateParenthesis(n - i -1)) {
+                    ans.push_back("(" + first +")"+ second);
+                }
+            }
+        }
+        return ans; 
+    }
+};

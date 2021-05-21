e052ec72ba5ec04e18c384af72bf076929f5f55b
2hard4me <usmanov.aa@phystech.edu>
2021-05-21 19:03:56 +0300

    Solved Merge_Intervals problem

--- Top 100 Interview Questions/#56_Merge_Intervals.cpp
+++ Top 100 Interview Questions/#56_Merge_Intervals.cpp
@@ -1,1 +1,19 @@
-
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(),intervals.end());
+        vector<vector<int>>ans;
+        int l = intervals[0][0], r = intervals[0][1];
+        for(int i = 1; i < intervals.size(); ++i) {
+            if(intervals[i][0] > r) {
+                ans.push_back({l,r});
+                l = intervals[i][0];
+                r = intervals[i][1];
+            }else{
+                r = max(intervals[i][1], r); 
+            }
+        }
+        ans.push_back({l, r});
+        return ans;
+    }
+};

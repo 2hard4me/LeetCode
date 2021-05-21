419d36c1639c31b12e130843efe60d7c308764b1
Rakhmatov Sunat <44374413+ssssunat@users.noreply.github.com>
2021-05-21 20:38:36 +0300

    Solved Missing_Number problem

--- Top 100 Interview Questions/#268_Missing_Number.cpp
+++ Top 100 Interview Questions/#268_Missing_Number.cpp
@@ -1,1 +1,9 @@
-
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int  n = nums.size(), s = 0;
+        for(auto& x : nums)
+            s += x;
+        return n*(n+1)/2 - s;
+    }
+};

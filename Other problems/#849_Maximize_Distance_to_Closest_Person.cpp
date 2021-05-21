6dd8e99c5c4a4c3c1896ff4be1ccdbc2bee02950
Rakhmatov Sunat <44374413+ssssunat@users.noreply.github.com>
2021-05-21 21:09:05 +0300

    Solved Maximize_Distance_to_Closest_Person problem

--- Other problems/#849_Maximize_Distance_to_Closest_Person.cpp
+++ Other problems/#849_Maximize_Distance_to_Closest_Person.cpp
@@ -1,1 +1,24 @@
-
+class Solution {
+public:
+    int maxDistToClosest(vector<int>& seats) {
+        int ans = 0, cnt = 0, j = 0;
+        while(!seats[j] && j < seats.size())
+            ++j;
+        ans = max(ans, j);
+        j = seats.size() - 1;
+        while(!seats[j] && j >= 0)
+            --j;
+        j = seats.size() - j - 1;
+        ans = max(ans, j);
+        
+        for(int i = 0; i < seats.size(); ++i) {
+            if(seats[i] == 0)
+                ++cnt;
+            else {
+                ans = max(ans, (cnt+1)/2);
+                cnt = 0;
+            }
+        }
+        return ans ;
+    }
+};

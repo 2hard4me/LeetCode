5232f7cb35140d9cbef1a1117a3e971ebe979e54
2hard4me <usmanov.aa@phystech.edu>
2021-05-21 19:15:08 +0300

    Solved Best_Time_to_Buy_and_Sell_Stock1 problem

--- Top 100 Interview Questions/#121_Best_Time_to_Buy_and_Sell_Stock1.cpp
+++ Top 100 Interview Questions/#121_Best_Time_to_Buy_and_Sell_Stock1.cpp
@@ -1,1 +1,15 @@
-
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int profit = 0,minprices =1e5+1;
+        for(int i =0; i < prices.size(); ++i){
+            if(minprices > prices[i])
+                minprices = prices[i];
+            else{
+                if(profit < prices[i] - minprices)
+                    profit = prices[i] - minprices;
+            }
+        }
+        return profit;
+    }
+};

d5964fa6881b980acdab69b75d632942f311cf42
Rakhmatov Sunat <44374413+ssssunat@users.noreply.github.com>
2021-05-21 20:37:57 +0300

    Solved Valid_Anagram problem

--- Top 100 Interview Questions/#242_Valid_Anagram.cpp
+++ Top 100 Interview Questions/#242_Valid_Anagram.cpp
@@ -1,1 +1,12 @@
-
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        map<char,int> m;
+        map<char,int> m1;
+        for(auto x : s)
+            ++m[x];
+        for(auto x : t)
+            ++m1[x];
+        return (m1 == m);
+    }
+};

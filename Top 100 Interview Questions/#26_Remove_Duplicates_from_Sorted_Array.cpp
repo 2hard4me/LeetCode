1de99e2885c9bce636b0b5f37881751bd3b8371c
2hard4me <usmanov.aa@phystech.edu>
2021-05-21 19:00:18 +0300

    Solved Remove_Duplicates_from_Sorted_Array problem

--- Top 100 Interview Questions/#26_Remove_Duplicates_from_Sorted_Array.cpp
+++ Top 100 Interview Questions/#26_Remove_Duplicates_from_Sorted_Array.cpp
@@ -1,1 +1,15 @@
-
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int j = 0;
+        for(int i = 0; i < nums.size(); ++i) {
+            while(i + 1 < nums.size() && nums[i] == nums[i+1]) {
+                 ++i;
+            }
+            nums[j] = nums[i];
+            ++j;
+        }
+        nums.erase(nums.begin() + j,nums.end());
+        return nums.size();
+    }
+};

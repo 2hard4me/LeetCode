4923d773bab34c77877d506803db9812fafb7b67
2hard4me <usmanov.aa@phystech.edu>
2021-05-21 19:01:08 +0300

    Solved Permutations problem

--- Top 100 Interview Questions/#46_Permutations.cpp
+++ Top 100 Interview Questions/#46_Permutations.cpp
@@ -1,1 +1,33 @@
-
+class Solution {
+public:
+    vector<int> v;
+    vector<int> v1;
+    vector<bool> used;
+    vector< vector<int> > s;
+    int n;
+    void rec(int idx){
+        if(idx == n){
+            vector<int>ans(n);
+            for(int i = 0;i < n; ++i)
+                ans[i] = v1[v[i]-1];
+            s.push_back(ans);
+            return;
+        }
+        for(int i = 1;i <= n; ++i){
+            if(used[i])
+                continue;
+            v[idx] = i;
+            used[i] = true;
+            rec(idx+1);
+            used[i] = false;
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        n = nums.size();
+        v = vector<int>(n);
+        v1 = nums;
+        used = vector<bool>(n+1,false);
+        rec(0);
+        return s;
+    }
+};

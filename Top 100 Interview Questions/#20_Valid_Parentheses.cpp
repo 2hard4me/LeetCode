71392a5af81ed03c0c9fc068569a417c9b7b3a6a
2hard4me <usmanov.aa@phystech.edu>
2021-05-21 18:58:01 +0300

    Solved Valid_Parentheses problem

--- Top 100 Interview Questions/#20_Valid_Parentheses.cpp
+++ Top 100 Interview Questions/#20_Valid_Parentheses.cpp
@@ -1,1 +1,23 @@
-
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> valid;
+        int bal = 0;
+        for(int i = 0; i < s.size(); ++i) {
+            if(s[i] == '{' || s[i] == '(' || s[i] == '['){
+                valid.push(s[i]);
+                ++bal;
+            } else {
+                --bal;
+                if(valid.empty())
+                    return false;
+                if(valid.top() == '(' && s[i] == '}' || valid.top() == '(' && s[i] == ']' || 
+                   valid.top() == '[' && s[i] == ')' || valid.top() == '[' && s[i] == '}' ||
+                   valid.top() == '{' && s[i] == ')' || valid.top() == '{' && s[i] == ']')
+                    return false;
+                valid.pop();
+            }
+        }
+        return bal == 0;
+    }
+};

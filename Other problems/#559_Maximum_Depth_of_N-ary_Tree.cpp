0a769a31ce6ddd5289aa826d7970e0f0b36b4996
Rakhmatov Sunat <44374413+ssssunat@users.noreply.github.com>
2021-05-21 21:05:54 +0300

    Solved Maximum_Depth_of_N-ary_Tree problem

--- Other problems/#559_Maximum_Depth_of_N-ary_Tree.cpp
+++ Other problems/#559_Maximum_Depth_of_N-ary_Tree.cpp
@@ -1,1 +1,41 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
 
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    int maxDepth(Node* root) {
+        if(!root)
+            return 0;
+        queue<Node*>q;
+        q.push(root);
+        int d = 0;
+        while(!q.empty()) {
+            int len = q.size();
+            for(int i = 0; i  < len; ++i) {
+                Node* curr = q.front();
+                q.pop();
+                for(auto& x : curr->children)
+                    q.push(x);
+            }
+            ++d;
+        }
+        return d;
+    }
+};

40539beb92db54c08aabf388e537b16153cef15e
2hard4me <usmanov.aa@phystech.edu>
2021-05-21 19:03:04 +0300

    Solved Maximum_Subarray problem

--- Top 100 Interview Questions/#53_Maximum_Subarray.cpp
+++ Top 100 Interview Questions/#53_Maximum_Subarray.cpp
@@ -1,1 +1,12 @@
-
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int minp = 0, maxsum = nums[0],pref = 0;
+        for(int i = 0; i < nums.size(); ++i) {
+            pref += nums[i];
+            maxsum = max(maxsum,pref - minp);
+             minp = min(pref,minp);
+        }
+        return maxsum;
+    }
+};

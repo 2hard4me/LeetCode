cceb515b00d0403188b9f22acdc5e97f107928c2
2hard4me <usmanov.aa@phystech.edu>
2021-05-21 19:02:04 +0300

    Solved Group_Anagrams problem

--- Top 100 Interview Questions/#49_Group_Anagrams.cpp
+++ Top 100 Interview Questions/#49_Group_Anagrams.cpp
@@ -1,1 +1,16 @@
-
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> ans;
+        unordered_map<string,vector<string>> m;
+        for(int i = 0; i < strs.size(); ++i) {
+            string s(26,'0');
+           for(char& c : strs[i])
+               ++s[c - 'a'];
+           m[s].push_back(strs[i]); 
+        }
+        for(auto &x : m)
+            ans.push_back(x.second);
+        return ans;
+    }
+};
